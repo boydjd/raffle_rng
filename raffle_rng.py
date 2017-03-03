85bd706607e1e1fe0bdeba0b57d105cc3c3c14cd
Eric Thomas Bostrom <eric@ericthomasbostrom.com>
2017-03-03 19:11:49 +0000

    v1.3

--- raffle_rng.py
+++ raffle_rng.py
@@ -8,9 +8,6 @@ print "STARTING RAFFLE-BOT..."
 # Current version
 version = "1.3"
 
-# Change the botname to the reddit username of your bot
-#botname="/u/raffle_rng_test"
-
 # Modify the user agent string to something unique
 user_agent='raffle_rng bot 1.0 by diversionmary make this unique'
 
@@ -20,9 +17,6 @@ reddit = praw.Reddit('botname', user_agent=user_agent)
 # Automatically grabs the username of the bot
 username = str(reddit.user.me())
 
-# If you're not using praw.ini uncomment the next line and fill in the authentication values:
-#reddit = praw.Reddit(user_agent=user_agent, client_id='put client_id here', client_secret='put client_secret here', username='put reddit username here', password='put password here')
-			
 # Error reply message
 error_reply = "Call the bot with total slots greater than 0 like this: " + username + " 20 \n \n *View the " + version + " source code at https://github.com/diversionmary/raffle_rng*"
 
@@ -34,9 +28,6 @@ def parse_to_integer(string):
     except ValueError:
 	pass
     return num_slots
-
-# If you want this script to be a TSR uncomment the next line. Make sure you indent all lines beginning with "for mention..."
-#while True:
 
 # Get unread mentions
 for mention in reddit.inbox.unread(limit=None):
@@ -58,12 +49,8 @@ for mention in reddit.inbox.unread(limit=None):
 			pass
 	# Randomly determine a winner from the slot range and reply
 	if total_slots != 0:
-		winner = random.randint(1, total_slots)
-		raffle_reply = "The winner is: " + str(winner) + "\n \n *View the " + version + " source code at https://github.com/diversionmary/raffle_rng*"
+		winner = str(random.randint(1, total_slots))
+		raffle_reply = "The winner is: " + winner + "\n \n *View the " + version + " source code at https://github.com/diversionmary/raffle_rng*"
 		mention.reply(raffle_reply)
-		print(raffle_reply)
+		print(winner)
 		mention.mark_read()
-
-# If you want this script to be a TSR uncomment the next two lines and indent them once as noted above.
-#print "Starting 10 minute sleep before next run."
-#time.sleep(600)
