f72cfe373d388399c745c4a3ba78aac8334bb755
Eric Thomas Bostrom <eric@ericthomasbostrom.com>
2017-03-03 18:56:04 +0000

    v1.3

--- raffle_rng.py
+++ raffle_rng.py
@@ -5,8 +5,11 @@ import random
 
 print "STARTING RAFFLE-BOT..."
 
+# Current version
+version = "1.3"
+
 # Change the botname to the reddit username of your bot
-botname="/u/raffle_rng_test"
+#botname="/u/raffle_rng_test"
 
 # Modify the user agent string to something unique
 user_agent='raffle_rng bot 1.0 by diversionmary make this unique'
@@ -14,11 +17,14 @@ user_agent='raffle_rng bot 1.0 by diversionmary make this unique'
 # If you're authenticating using praw.ini uncomment the next line:
 reddit = praw.Reddit('botname', user_agent=user_agent)
 
+# Automatically grabs the username of the bot
+username = str(reddit.user.me())
+
 # If you're not using praw.ini uncomment the next line and fill in the authentication values:
 #reddit = praw.Reddit(user_agent=user_agent, client_id='put client_id here', client_secret='put client_secret here', username='put reddit username here', password='put password here')
 			
 # Error reply message
-error_reply = "Call the bot with total slots greater than 0 like this: " + botname + " 20 \n \n *View the source code at https://github.com/diversionmary/raffle_rng*"
+error_reply = "Call the bot with total slots greater than 0 like this: " + username + " 20 \n \n *View the " + version + " source code at https://github.com/diversionmary/raffle_rng*"
 
 # parse_to_integer turns strings into integers
 def parse_to_integer(string):
@@ -26,7 +32,7 @@ def parse_to_integer(string):
     try:
         num_slots = int(string)
     except ValueError:
-        pass
+	pass
     return num_slots
 
 # If you want this script to be a TSR uncomment the next line. Make sure you indent all lines beginning with "for mention..."
@@ -36,18 +42,10 @@ def parse_to_integer(string):
 for mention in reddit.inbox.unread(limit=None):
 	# Turn the body of the message  into a list of strings
 	comment_word_list = mention.body.split(" ")
-	# If the first item in the list isn't the bot's name, break
-	if mention.body.split(" ")[0] <> botname:
-		mention.reply(error_reply)
-		print "botname trap"
-		print mention.body.split(" ")[0]
-		mention.mark_read()
-		break
-	# If the body isn't two items (the bot's name and an integer), reply with instructions then break
-	elif len(comment_word_list) !=2:
-		mention.reply(error_reply)
-		print "length trap"
-		print len(comment_word_list)
+	# If the first item in the list isn't the bot's name, and the second item isn't an integer, break and mark as read
+	if len(comment_word_list) >2 or comment_word_list[0] !=username and parse_to_integer(comment_word_list[1]) <= 0:
+		print "Not a raffle call"
+		print comment_word_list[1]
 		mention.mark_read()
 		break
 	total_slots = 0
@@ -61,7 +59,7 @@ for mention in reddit.inbox.unread(limit=None):
 	# Randomly determine a winner from the slot range and reply
 	if total_slots != 0:
 		winner = random.randint(1, total_slots)
-		raffle_reply = "The winner is: " + str(winner) + "\n \n *View the source code at https://github.com/diversionmary/raffle_rng*"
+		raffle_reply = "The winner is: " + str(winner) + "\n \n *View the " + version + " source code at https://github.com/diversionmary/raffle_rng*"
 		mention.reply(raffle_reply)
 		print(raffle_reply)
 		mention.mark_read()
