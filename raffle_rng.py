8c10fa001206c1f3e3eddce4ee1c85c90043156f
Eric Thomas Bostrom <eric@ericthomasbostrom.com>
2017-04-05 16:28:29 +0000

    1.6

--- raffle_rng.py
+++ raffle_rng.py
@@ -6,7 +6,7 @@ import random
 print "STARTING RAFFLE-BOT..."
 
 # Current version
-version = "1.5"
+version = "1.6"
 
 # Modify the user agent string to something unique
 user_agent="raffle_rng bot" + version + " by diversionmary make this part unique"
@@ -31,41 +31,37 @@ def parse_to_integer(string):
 # Get unread mentions
 for mention in reddit.inbox.unread(limit=None):
 	
-	# Get first two words from body of message if available
-	comment_word_list = mention.body.split(" ")
-	word1 = str(comment_word_list[0])
-	if word1 == username and len(comment_word_list) >= 2:
-		word2 = parse_to_integer(comment_word_list[1])
+    # Get first two words from body of message if available
+    comment_word_list = mention.body.split(" ")
+    word1 = str(comment_word_list[0])
+    total_slots = 0
+    if word1 == username and len(comment_word_list) == 2:
+        total_slots = parse_to_integer(comment_word_list[1])
 	
 	# If less than two words, or if the first word isn't our bot's reddit username, mark message read and move on
-	elif word1 != username:
-		print "Not a raffle call, ignore"
-		mention.mark_read()
-		break
+    elif word1 != username:
+        print "Not a raffle call, ignore"
+	mention.mark_read()
+	break
 
 	# If the first item in the list is only the bot's name reply with instructions
-	if word1 == username and len(comment_word_list) < 2:
-		print "Bad raffle call, reply"
-		mention.reply(error_reply)
-		mention.mark_read()
-		break
+    if word1 == username and len(comment_word_list) < 2:
+	print "Bad raffle call, reply"
+	mention.reply(error_reply)
+	mention.mark_read()
+	break
 
 	# If the first item in the list is the bot's name, and the slot number is invalid, reply with instructions
-	elif word1 == username and word2 <= 0:
-		print "Bad raffle call, reply"
-		mention.reply(error_reply)
-		mention.mark_read()
-		break
-	
-	# If we made it this far, turn word2 into total slots
-	total_slots = 0
-	total_slots = word2
+    elif word1 == username and total_slots <= 0:
+	print "Bad raffle call, reply"
+	mention.reply(error_reply)
+	mention.mark_read()
+	break
 	
 	# Randomly determine a winner from the slot range and reply
-	if total_slots > 0:
-		winner = str(random.randint(1, total_slots))
-		raffle_reply = "The winner is: " + winner + "\n \n *View the " + version + " source code at https://github.com/diversionmary/raffle_rng*"
-		mention.reply(raffle_reply)
-		print(winner)
-		mention.mark_read()
-	
+    if total_slots > 0:
+	winner = str(random.randint(1, total_slots))
+	raffle_reply = "The winner is: " + winner + "\n \n *View the " + version + " source code at https://github.com/diversionmary/raffle_rng*"
+	mention.reply(raffle_reply)
+	print(winner)
+	mention.mark_read()
