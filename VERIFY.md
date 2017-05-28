0c85bb6363717b80acfefb40ef26f34206708e70
Evan Johnson <evan@slicklabs.io>
2017-05-28 17:20:30 -0500

    add a way to test my work and verify I'm doing what I said

---
+++ VERIFY.md
@@ -0,0 +1,6 @@
+# How to verify the hash
+
+1. Download and install `python`
+1. Download `verify.py` from this repository
+1. Replace `seed` with the seed you're testing
+1. Replace `slots` with the maximum slots
---
+++ verify.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python
+
+# Once you enter the seed and number of slots
+# you should be able to run this script repeatedly
+# and get he same result every time you run it
+
+import random
+
+# Put the number of slots here
+# Used as a modulus and max calculation
+# of random number
+slots = 20
+
+# the seed you're testing
+seed = 'PASTE_SEED_HERE'
+
+# This sets the seed that python uses to 
+# generate the random number
+random.seed(seed)
+
+# This is our actual random number
+# Minimum is always 1
+# Maximum is one million times number of slots
+# which will give an even distribution chance
+big_result = random.randint(1, slots * 1000000)
+
+# The winning number is the big random number 
+# modulus the number of slots plus one
+# Modulus would normally return 0 to SLOTS-1
+# but the + 1 makes it return 1 to SLOTS
+# which is what we want
+winner = big_result % slots + 1
+
+response = """
+The winner is {}
+Seed: {}
+Random Number: {}
+Modulus (slots): {}
+""".format(winner, seed, big_result, slots)
+
+print(response)
